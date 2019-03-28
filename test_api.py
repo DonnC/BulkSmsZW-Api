ca2d3f1bab990526143111d52619bff29e779e26
Donald Chinhuru <donychinhuru@gmail.com>
2019-03-28 23:35:23 +0200

    run tests on api call

--- test_api.py
+++ test_api.py
@@ -11,9 +11,10 @@ bulksms = Client(username=BULKSMS_NAME, token=BULKSMSWEB_KEY)
 
 # try sending
 try:
+    # set "credit" flag = True so as to get text sms left on your web portal
     print("Sending..")
-    resp = bulksms.send(body="hello, api test 2", recipients=['263782897884'], credits=True)
+    resp = bulksms.send(body="hello, bulksmszw api test run", recipients=['2637xxxxxxxx'], credits=True)
     print(resp)
 
 except Exception as exc:
-    print("Error: %s" %exc)
+    print("Error: %s" % exc)
