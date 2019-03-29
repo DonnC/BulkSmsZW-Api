548bc1db3443efb098db483a322a2d0549038d26
Donald Chinhuru <donychinhuru@gmail.com>
2019-03-29 21:16:59 +0200

    added private data members and methods

--- BulkSmsApi/Client.py
+++ BulkSmsApi/Client.py
@@ -9,39 +9,39 @@ class Client:
         client to connect to bulksms zw services and send a text message
     """
 
-    BULKSMS_WEBSERVICE_URL = "http://portal.bulksmsweb.com/index.php?app=ws"
-    SEND_SMS_OPERATION     = "pv"
-    SMS_CREDIT_OPERATION   = "cr"
+    __BULKSMS_WEBSERVICE_URL = "http://portal.bulksmsweb.com/index.php?app=ws"
+    __SEND_SMS_OPERATION     = "pv"
+    __SMS_CREDIT_OPERATION   = "cr"
     
     def __init__(self, username=None, token=None):
         self.username = username
         self.token    = token
         logging.basicConfig(level="INFO")
 
-    def txt_op(self):
+    def __txt_op(self):
         # return url with username and token for text operation only
         if self.username and self.token:
             txt_op_details = "&u={user}&={key}&op={operation}".format(
                 user=self.username,
                 key=self.token, 
-                operation=self.SEND_SMS_OPERATION)
+                operation=self.__SEND_SMS_OPERATION)
             
-            web_service_string = self.BULKSMS_WEBSERVICE_URL + txt_op_details
+            web_service_string = self.__BULKSMS_WEBSERVICE_URL + txt_op_details
             
             return web_service_string
 
         else:
             raise Exception("Username and Token not provided")
 
-    def credit(self):
+    def __credit(self):
         # url for credit retrieval
         if self.username and self.token:
             credit_details = "&u={user}&h={key}&op={operation}".format(
                 user=self.username,
                 key =self.token, 
-                operation=self.SMS_CREDIT_OPERATION)
+                operation=self.__SMS_CREDIT_OPERATION)
             
-            web_service_string = self.BULKSMS_WEBSERVICE_URL + credit_details
+            web_service_string = self.__BULKSMS_WEBSERVICE_URL + credit_details
             
             return web_service_string
 
@@ -67,9 +67,9 @@ class Client:
         else:
             raise Exception("Recipients should be passed as a list")
 
-    def api_errors(self, error_response):
+    def __api_errors(self, error_response):
         # handle bulksmszw api error
-        if not error_response.get("error_string"):
+        if error_response.get("error_string") != "None":
             return True
 
     def send_request(self, text, to, operation):
@@ -82,13 +82,12 @@ class Client:
         try:
             req.raise_for_status()
             response = req.json()
-            if self.api_errors(error_response=response):
+            if self.__api_errors(error_response=response):
                 raise Exception(response)
             
             return response
 
         except Exception as error:
-            print("There was an error sending text request: %s" %error)
             return error
 
     def send(self, body, recipients, credits=False, schedule=None):
@@ -97,9 +96,9 @@ class Client:
         recipients = self.recipients(recipients_list=recipients)
         
         if credits:
-            resp = self.send_request(text=body, to=recipients, operation=self.credit())
+            resp = self.send_request(text=body, to=recipients, operation=self.__credit())
             
         else:
-            resp = self.send_request(text=body, to=recipients, operation=self.txt_op())
+            resp = self.send_request(text=body, to=recipients, operation=self.__txt_op())
 
         return resp
