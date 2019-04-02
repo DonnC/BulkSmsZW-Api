a33d654d78592ede633a2127dac0ee9a5fc54059
Donald Chinhuru <donychinhuru@gmail.com>
2019-04-03 01:20:32 +0200

    update Parse methods

--- BulkSmsApi/Parse.py
+++ BulkSmsApi/Parse.py
@@ -5,7 +5,7 @@ from urllib.parse import quote
 
 class Parse(object):
     '''
-        parse and encode given url
+        parse and encode webservice payload
     '''
     def __init__(self, web_url, quote_body, quote_recipients):
         self.web_url          = web_url
@@ -13,20 +13,20 @@ class Parse(object):
         self.quote_recipients = quote_recipients
 
     def url(self):
-        return self.web_url + self.payload()
+        return self.web_url + self.__payload()
 
-    def body(self):
+    def __body(self):
         '''quote body of text '''
         return quote(self.quote_body)
 
-    def recipients(self):
+    def __recipients(self):
         '''quote recipients list'''
         return quote(self.quote_recipients)
 
-    def payload(self):
+    def __payload(self):
         '''return payload'''
         url_payload = ""
-        url_payload += "&to=" + self.recipients()
-        url_payload += "&msg=" + self.body()
+        url_payload += "&to=" + self.__recipients()
+        url_payload += "&msg=" + self.__body()
 
         return url_payload
